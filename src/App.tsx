676a6c014a279b3a86ff809d1ac72ae5c86f76ed
rafizul896 <rafizulislam896@gmail.com>
2025-01-01 17:18:58 +0600

    add: Write Actions in Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-10">
       <h1 className="text-3xl font-semibold">Counter with Redux</h1>
       <div className="flex gap-10 mt-10">
-        <button className="border-2 p-3 rounded-md font-medium hover:bg-gray-200">
-          Increment
+        <button
+          onClick={handleDecrement}
+          className="border-2 p-3 rounded-md font-medium hover:bg-gray-200"
+        >
+          Decrement
         </button>
         <div className="border-2 px-4 pt-3 rounded-full font-medium">0</div>
-        <button className="border-2 p-3 rounded-md font-medium hover:bg-gray-200">
-          Decrement
+        <button
+          onClick={handleIncrement}
+          className="border-2 p-3 rounded-md font-medium hover:bg-gray-200"
+        >
+          Increment
         </button>
       </div>
     </div>
