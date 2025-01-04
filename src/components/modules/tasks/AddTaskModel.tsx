4bf3cf76fab52d9200e09e8137fed6dd5b6011a3
rafizul896 <rafizulislam896@gmail.com>
2025-01-04 09:15:47 +0600

    add: the Add Task Form

--- src/components/modules/tasks/AddTaskModel.tsx
+++ src/components/modules/tasks/AddTaskModel.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Dialog,
   DialogContent,
@@ -17,6 +18,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
   Select,
   SelectContent,
   SelectItem,
@@ -24,7 +30,10 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 import { DialogDescription } from "@radix-ui/react-dialog";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 
 export function AddTaskModel() {
@@ -58,7 +67,7 @@ export function AddTaskModel() {
                   <FormLabel>Title</FormLabel>
                   <FormControl>
                     <Input
-                      placeholder="shadcn"
+                      placeholder="Write a Title Name"
                       {...field}
                       value={field.value || ""}
                     />
@@ -76,7 +85,7 @@ export function AddTaskModel() {
                   <FormLabel>Description</FormLabel>
                   <FormControl>
                     <Textarea
-                      placeholder="shadcn"
+                      placeholder="Write description"
                       {...field}
                       value={field.value || ""}
                     />
@@ -85,23 +94,7 @@ export function AddTaskModel() {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="dueDate"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Due Date</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="shadcn"
-                      {...field}
-                      value={field.value || ""}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {/* priority */}
             <FormField
               control={form.control}
               name="priority"
@@ -109,9 +102,12 @@ export function AddTaskModel() {
                 <FormItem>
                   <FormLabel>Priority</FormLabel>
                   <FormControl>
-                    <Select {...field}>
-                      <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Selete Priopity" />
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selete a priopity to set" />
                       </SelectTrigger>
                       <SelectContent>
                         <SelectItem value="Low">Low</SelectItem>
@@ -124,6 +120,49 @@ export function AddTaskModel() {
                 </FormItem>
               )}
             />
+            {/* dueDate */}
+            <FormField
+              control={form.control}
+              name="dueDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Due Date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            " pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        // disabled={(date) =>
+                        //   date > new Date() || date < new Date("1900-01-01")
+                        // }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* submit button */}
             <DialogFooter className="mt-5">
               <Button type="submit">Save changes</Button>
             </DialogFooter>
